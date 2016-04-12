e60fbc7ad7af5ec91835ec6de81dc0142271f0a8
taka66 <taka66.iphone@gmail.com>
2016-04-12 15:37:35 +0900

    Change tumbnail photos and title.

---
+++ js/map.js
@@ -0,0 +1,26 @@
+$(function() {
+
+	var canvas = document.getElementById( 'map-canvas' ) ;
+	var latlng = new google.maps.LatLng( 35.628743 , 139.685247 );
+	var markers = [] ;
+	// options
+	var mapOptions = {
+		zoom: 13 ,
+		center: latlng ,
+		draggable: false ,
+	};
+
+	var map = new google.maps.Map( canvas , mapOptions ) ;
+
+	// generate marker
+	// Gakugeidaigaku station
+	markers[0] = new google.maps.Marker({
+		map: map ,
+		position: new google.maps.LatLng( 35.628743 , 139.685247 ) ,
+	}) ;
+	// Shibuya station
+	markers[1] = new google.maps.Marker({
+		map: map ,
+		position: new google.maps.LatLng( 35.659126 , 139.701936 ) ,
+	}) ;
+});
